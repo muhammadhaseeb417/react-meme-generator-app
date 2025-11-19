4f18ae1955c9bafeadd8241395822c93992cd6af
Muhammad Haseeb Amjad <muhammadhaseebamjad.dev@example.com>
2025-11-19 21:35:46 +0500

    now displaying the memes  properilly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react"
 import MemeCard from "./components/meme-card"
+import { getAllMemes } from "./api/memes"
+import './App.css'
 
 function App() {
 
+  const [memes, setMemes] = useState([])
+
+  useEffect(() => {
+    getAllMemes().then(memes => setMemes(memes.data.memes))
+  }, [])
+
+  console.log(memes)
+
   return (
     <>
-      <MemeCard />
+      {
+        memes.map(meme => <MemeCard key={meme.id} src={meme.url} title={meme.name} />)
+      }
     </>
   )
 }
--- src/components/meme-card.jsx
+++ src/components/meme-card.jsx
@@ -1,16 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function MemeCard() {
+function MemeCard(props) {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+        <Card style={{ width: '18rem' }} className='card-meme'>
+            <Card.Img variant="top" src={props.src} style={{ height: "200px" }} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
+                <Card.Title>{props.title}</Card.Title>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
         </Card>
