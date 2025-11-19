3f309401b7ee61640bfa0b952c3dcf10cac08bf4
Muhammad Haseeb Amjad <muhammadhaseebamjad.dev@example.com>
2025-11-19 22:25:08 +0500

    now adding text onto the screen when pressing add text in edit page

---
+++ src/components/add-text.jsx
@@ -0,0 +1,9 @@
+function AddText() {
+    return (
+        <div>
+            <h2>Double click to edit text</h2>
+        </div>
+    )
+}
+
+export default AddText
--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import { useSearchParams } from "react-router"
+import AddText from "../components/add-text"
 
 function EditPage() {
     const [params] = useSearchParams()
     const imgURL = params.get("url")
+    const [count, setCount] = useState(0)
+
     return (
         <div >
-            <h1>Edit Your Meme</h1>
-            <img src={imgURL} alt="meme" style={{ height: "400px" }} />
-            <button >Add Text</button>
+            <div>
+                <h1>Edit Your Meme</h1>
+                <img src={imgURL} alt="meme" style={{ height: "400px" }} />
+                {
+                    Array(count).fill(0).map(e => <AddText />)
+                }
+            </div>
+            <button onClick={() => setCount(count + 1)}>Add Text</button>
         </div>
     )
 }
