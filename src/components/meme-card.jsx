bec32c8b706a9a640d6b2b1184cabd903372fd31
Muhammad Haseeb Amjad <muhammadhaseebamjad.dev@example.com>
2025-11-19 22:19:43 +0500

    displaying img in other page ussing react router use serach router

--- src/components/meme-card.jsx
+++ src/components/meme-card.jsx
@@ -1,13 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router';
+
+
 
 function MemeCard(props) {
+    const navigator = useNavigate()
+
     return (
         <Card style={{ width: '18rem' }} className='card-meme'>
             <Card.Img variant="top" src={props.src} style={{ height: "200px" }} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
-                <Button variant="primary">Edit Meme</Button>
+                <Button onClick={(e) => navigator(`/edit?url=${props.src}`)} variant="primary">Edit Meme</Button>
             </Card.Body>
         </Card>
     );
--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,7 +1,13 @@
+import { useSearchParams } from "react-router"
+
 function EditPage() {
+    const [params] = useSearchParams()
+    const imgURL = params.get("url")
     return (
-        <div>
+        <div >
             <h1>Edit Your Meme</h1>
+            <img src={imgURL} alt="meme" style={{ height: "400px" }} />
+            <button >Add Text</button>
         </div>
     )
 }
